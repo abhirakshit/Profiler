670545ad7def5698106a240ffcbd76637da56eaa
Abhishek Rakshit <abhishek06161@gmail.com>
2014-01-20 22:32:49 -0500

    Added requireJS key and optimization code to build

--- project/Build.scala
+++ project/Build.scala
@@ -20,6 +20,7 @@ object ApplicationBuild extends Build {
     // Add your own project settings here
     requireJs += "app.js"
     //requireNativePath := Some("C:\Users\rabhishe\AppData\Roaming\npm\node_modules\requirejs\bin\r.js")
+    requireNativePath := Some("/C/Users/rabhishe/AppData/Roaming/npm/node_modules/requirejs/bin/r.js")
      //requireJsShim += "app.js"
   )
 
