45906906edd331249cc3e49d8fa84e67c4fffd13
Abhishek Rakshit <abhishek06161@gmail.com>
2014-01-20 23:08:31 -0500

    Added requireJS key and optimization code to build

--- project/Build.scala
+++ project/Build.scala
@@ -19,9 +19,9 @@ object ApplicationBuild extends Build {
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
     requireJs += "app.js",
-    //requireNativePath := scala.util.Properties.envOrNone("REQUIREJS_PATH")
+    requireNativePath := scala.util.Properties.envOrNone("REQUIREJS_PATH")
     //requireNativePath := Some("C:\Users\rabhishe\AppData\Roaming\npm\node_modules\requirejs\bin\r.js")
-    requireNativePath := Some("C:/Users/rabhishe/AppData/Roaming/npm/node_modules/requirejs/bin/r.js")
+    //requireNativePath := Some("C:/Users/rabhishe/AppData/Roaming/npm/node_modules/requirejs/bin/r.js")
      //requireJsShim += "app.js"
   )
 
