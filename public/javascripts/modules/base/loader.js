079cfd1f7e07ab61da1f1bd99428b9babcff2f95
Abhishek Rakshit <abhishek06161@gmail.com>
2014-01-21 21:52:54 -0500

    Fix requirejs issue

--- public/javascripts/modules/base/loader.js
+++ public/javascripts/modules/base/loader.js
@@ -39,7 +39,32 @@ var dependencies = [
     "modules/base/controller"
 ];
 
-define(dependencies, function(){
+//define(dependencies,
+define([
+
+    //Libs
+    "dataTables",
+    "dateTimePicker",
+//    "backboneValidation",
+//    "backboneSyphon",
+
+    //Models
+    "modules/base/models/base",
+    "modules/base/models/multiSelect",
+
+    //Views
+    "modules/base/views/layouts/layouts",
+    "modules/base/views/tables/tables",
+    "modules/base/views/forms/formElems",
+    "modules/base/views/headers/pageHeader",
+    "modules/base/views/navigation/navTabs",
+    "modules/base/views/validation/validationMessages",
+
+    //Controller
+    "modules/base/controller"
+],
+
+    function(){
 //    console.log(module.config().user)
     Application.module("Base", function(Base, Application, Backbone, Marionette, $, _){
         Application.addInitializer(function(){
